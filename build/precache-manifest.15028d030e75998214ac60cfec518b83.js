2315a57fcf62e02a8d8d59b47990499292caf520
Max Alva <alva85@gmail.com>
2018-11-28 01:02:53 -0500

    build

---
+++ build/precache-manifest.15028d030e75998214ac60cfec518b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2effd993577640d112b3",
+    "url": "/static/js/main.2effd993.chunk.js"
+  },
+  {
+    "revision": "5e599bcf4900aaf4206a",
+    "url": "/static/js/1.5e599bcf.chunk.js"
+  },
+  {
+    "revision": "2effd993577640d112b3",
+    "url": "/static/css/main.3fba3b5b.chunk.css"
+  },
+  {
+    "revision": "102db76567429e061da7ff6352bceb89",
+    "url": "/index.html"
+  }
+];

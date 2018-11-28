af7131fe3a6c76571c59c9542c27f7b47057c5e5
Max Alva <alva85@gmail.com>
2018-11-28 00:18:37 -0500

    Hlpers para cotizar el seguro

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Form from './Form';
+import { obtenerDiferenciaAnio, calcularMarca, obtenerPlan } from '../helpers/helper'
 
 class App extends Component {
 
-    cotizarSeguro = () => {
+    cotizarSeguro = (data) => {
+        const { marca, plan, year } = data;
+
+        let result = 2000;
+
+        const diff = obtenerDiferenciaAnio(year);
+
+        result -= ((diff * 3) * result) / 100;
+
+        result = calcularMarca(marca) * result;
+
+        result = parseFloat(obtenerPlan(plan) * result).toFixed(2);
+
+        console.log(result);
 
     }
 
